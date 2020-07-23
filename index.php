8db5e876d5126a9529ef6580d4ae9148ce6c9fff
Aby Kal <abyabrkal@gmail.com>
2020-07-23 17:05:11 +0400

    Strings

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php 
- echo 'Hi from php'
+ echo 'Hi from php';
+
+ $name1 = "Joe";
+ $name2 = "Abraham";
+ $name3 = "Sarah";
+
+ echo $name1 . $name2;
+ echo "Name3 is" . $name3;
+
+ echo "Length of Name2:" . strlen($name2);
+ echo "All is \"WELL\"";
+
+
 ?>
 
 <!DOCTYPE html>
